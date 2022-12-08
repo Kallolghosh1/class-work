e50d8da113cb0cab22616e3dd67555c30086e908
Kallolghosh1 <120078846+Kallolghosh1@users.noreply.github.com>
2022-12-08 14:17:52 +0530

    Add files via upload

---
+++ REVERSEANO.java
@@ -0,0 +1,21 @@
+package kallol1;
+
+public class REVERSEANO {
+
+	public static void main(String[] args) {
+
+		int num1,sum=0;
+		System.out.println("Enter the number: ");
+		Scanner sc=new Scanner(System.in);
+		num1=sc.nextInt();
+		while(num1!=0)
+		{
+			int rem=num1%10;
+			sum=sum+rem;
+			num1=num1/10;	
+		}
+		System.out.println("sum of digits: "+sum);
+
+	}
+
+}
