ad14c3394d297d22f8642d4ed11e63e5cbd8e8dd
Kallolghosh1 <120078846+Kallolghosh1@users.noreply.github.com>
2022-12-08 13:41:20 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,24 @@
+
+package kallol1;
+import java.util.Scanner;
+public class sum {
+
+
+	 	public static void main(String[] args) {
+	 		int num1,count=0;
+			System.out.println("Enter the number: ");
+			Scanner sc=new Scanner(System.in);
+			num1=sc.nextInt();
+			while(num1!=0)
+			{
+				num1=num1/10;
+				count++;
+				
+			}
+			System.out.println("number of digits: "+count);
+	 		}
+}
+	 
+
+
+
