60309b3bf76d22ba7cedaf82fc44fb6677a641fb
Kallolghosh1 <120078846+Kallolghosh1@users.noreply.github.com>
2022-12-08 13:42:51 +0530

    Add files via upload

--- sum.java
+++ sum.java
@@ -5,19 +5,21 @@ public class sum {
 
 
 	 	public static void main(String[] args) {
-	 		int num1,count=0;
+	 		int num1,sum=0;
 			System.out.println("Enter the number: ");
 			Scanner sc=new Scanner(System.in);
 			num1=sc.nextInt();
 			while(num1!=0)
 			{
-				num1=num1/10;
-				count++;
-				
+				int rem=num1%10;
+				sum=sum+rem;
+				num1=num1/10;	
 			}
-			System.out.println("number of digits: "+count);
-	 		}
-}
+			System.out.println("sum of digits: "+sum);
+
+		}
+
+	}
 	 
 
 
